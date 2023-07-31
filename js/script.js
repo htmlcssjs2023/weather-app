59f834a7e4f865a38cfb49286ed9f7db968c68b3
mdabdulhalim <htmlcssjs2023@gmail.com>
2023-07-31 07:43:56 +0600

    Add weather api

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const apiKey =  "863242cfb2b1d357e6093d9a4df19a4b";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather? units=metric&q=";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
