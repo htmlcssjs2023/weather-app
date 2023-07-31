5f2d1ff39ad0adac564d03ee28ac9e739d1127c3
mdabdulhalim <htmlcssjs2023@gmail.com>
2023-07-31 06:57:43 +0600

    JS Added

---
+++ js/script.js
@@ -0,0 +1,58 @@
+const apiKey =  "863242cfb2b1d357e6093d9a4df19a4b";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather? units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+
+
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    if(response.status == 404){
+        document.querySelector(".errorMessage").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+    else{
+        var data = await response.json()
+    
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+    }
+    else if(data.weather[0].main == "Humidity"){
+        weatherIcon.src = "images/humidity.png";
+    }
+    else if(data.weather[0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";
+    }
+    else if(data.weather[0].main == "Snow"){
+        weatherIcon.src = "images/snow.png";
+    }
+    else if(data.weather[0].main == "Wind"){
+        weatherIcon.src = "images/wind.png";
+    }
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".errorMessage").style.display = "none";
+
+
+}
+
+    }
+    
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value);
+})
